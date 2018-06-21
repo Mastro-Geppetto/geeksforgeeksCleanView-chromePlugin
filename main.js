c35e1a69ce4f11792b1ce50d81b2f6d498b313cc
Promit Chatterjee <promit.chatterjee@gmail.com>
2018-06-21 15:56:08 +0530

    Change tab icon to stackoverflow

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+// Change the tab icon
+var icon = document.querySelectorAll("head link[rel='shortcut icon']")[0];
+//icon.setAttribute("href", "https://www.shareicon.net/download/16x16/2017/04/19/884518_planet_512x512.png");
+// use stackovreflow icon
+icon.setAttribute("href", "https://cdn.sstatic.net/Sites/stackoverflow/img/favicon.ico?v=4f32ecc8f43d");
+
+// Detox the document
 var whole = document.getElementsByTagName("body")[0];
 var content = document.getElementById("content");
 whole.innerHTML = content.innerHTML;
